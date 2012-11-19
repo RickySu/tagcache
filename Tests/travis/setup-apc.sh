8425fe1ed3cd1523cec384bb03b4c677df7d384a
Ricky Su <ricky@ez2.us>
2012-11-19 14:43:23 +0800

    update apc build script

--- Tests/travis/setup-apc.sh
+++ Tests/travis/setup-apc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pecl install apc
+echo "" | pecl install apc
 echo "extension=apc.so" >> `php --ini |grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 echo "apc.enabled=1" >> `php --ini |grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 echo "apc.enable_cli=1" >> `php --ini |grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
