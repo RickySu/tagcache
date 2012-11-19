bec68122daca5e246e3ad8c050731a2ca7dfdcf5
Ricky Su <ricky@ez2.us>
2012-11-19 17:20:40 +0800

    user apc beta version

--- tests/travis/setup-apc.sh
+++ tests/travis/setup-apc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "" | pecl install apc
+echo "" | pecl install apc-beta
 echo "extension=apc.so" >> `php --ini |grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 echo "apc.enabled=1" >> `php --ini |grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 echo "apc.enable_cli=1" >> `php --ini |grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
