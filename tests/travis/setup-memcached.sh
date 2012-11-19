9c90c1be3746d163753d7323a796356d243de97b
Ricky Su <ricky@ez2.us>
2012-11-19 16:54:44 +0800

    use ubuntu default libmemcached lib

--- tests/travis/setup-memcached.sh
+++ tests/travis/setup-memcached.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sudo apt-get install libcloog-ppl0
-wget -c 'http://launchpad.net/libmemcached/1.0/1.0.14/+download/libmemcached-1.0.14.tar.gz'
-tar -xvf libmemcached-1.0.14.tar.gz
-sh -c "cd libmemcached-1.0.14 && ./configure --disable-sasl && make all && sudo make install && sudo ldconfig"
+#wget -c 'http://launchpad.net/libmemcached/1.0/1.0.14/+download/libmemcached-1.0.14.tar.gz'
+#tar -xvf libmemcached-1.0.14.tar.gz
+#sh -c "cd libmemcached-1.0.14 && ./configure --disable-sasl && make all && sudo make install && sudo ldconfig"
+sudo apt-get install -y libmemcached-dev
 echo 'y' | pecl install memcached
 echo "extension=memcached.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 sudo ldconfig
