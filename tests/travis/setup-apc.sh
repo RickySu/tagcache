0de12975524b0b37a25d763d6199fb80095da280
Ricky Su <ricky@ez2.us>
2012-12-04 11:50:35 +0800

    remove shell script for security reason

--- tests/travis/setup-apc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "" | pecl install apc-beta
-echo "extension=apc.so" >> `php --ini |grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
-echo "apc.enabled=1" >> `php --ini |grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
-echo "apc.enable_cli=1" >> `php --ini |grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
-sudo ldconfig -v
--- tests/travis/setup-memcache.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo 'y' | pecl install memcache
-echo "extension=memcache.so" >> `php --ini |grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
-sudo ldconfig -v
